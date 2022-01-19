8cb46b8614a47dbb523b27240519f38a503dc625
Yves LMDG <yves.lmdg@sfr.fr>
2022-01-19 10:17:31 +0100

    TP 04

---
+++ tp04_json.js
@@ -0,0 +1,12 @@
+let entreprise = {
+    "Nom":"Google",
+    "Fondateurs":[
+    {"Nom":"Larry Page", "dateDeNaissance":"26/03/1973", 
+        "lieuDeNaissance": { "Ville":"East Lansing", "etat":"Michigan"}},
+    {"Nom":"Sergey Brin", "dateDeNaissance":"21/08/1973", 
+        "lieuDeNaissance": {"Ville":"Moscou", "etat":"Union Soviétique"}}],
+    "Chiffres d’affaires":{"unite":"milliards de $", "valeur":[
+    {"date":"2008","montant":16.49},
+    {"date":"2012","montant":37.97},
+    {"date":"2016","montant":89.46},
+    {"date":"2018","montant":136.2}]}};
